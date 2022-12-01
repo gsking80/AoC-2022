a5c7378fa4baffe993387179a6c3715cd69926b5
Greg King <greg.king@gfs.com>
2022-12-01 00:26:54 -0500

    2022 Day 01

--- src/main/java/king/greg/aoc2022/Day01.java
+++ src/main/java/king/greg/aoc2022/Day01.java
@@ -1,7 +1,37 @@
 package king.greg.aoc2022;
 
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Day01 {
 
+  private final List<Long> elves = new ArrayList<>();
+
+  Day01(final List<String> lines) {
+    long currCount = 0;
+    for (final String line : lines) {
+      if (line.isBlank()) {
+        elves.add(currCount);
+        currCount = 0;
+        continue;
+      }
+      currCount += Long.parseLong(line);
+    }
+  }
+
+  long topCalories() {
+    return elves.stream().mapToLong(v -> v).max().orElse(0);
+  }
+
+  long topThreeCalories() {
+    Collections.sort(elves);
+    long value = 0;
+    for (var i = 1; i <4 ; i++) {
+      value += elves.get(elves.size() - i);
+    }
+    return value;
+  }
 
 }
--- src/test/java/king/greg/aoc2022/Day01Test.java
+++ src/test/java/king/greg/aoc2022/Day01Test.java
@@ -1,5 +1,31 @@
 package king.greg.aoc2022;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
 public class Day01Test {
+
+  @Test
+  public void testSolution1() throws IOException, URISyntaxException {
+    final var lines = Files.readAllLines(Paths.get(
+        Objects.requireNonNull(getClass().getClassLoader().getResource("Day01/input.txt"))
+            .toURI()));
+    final Day01 day01 = new Day01(lines);
+    Assertions.assertThat(day01.topCalories()).isEqualTo(68292);
+  }
+
+  @Test
+  public void testSolution2() throws IOException, URISyntaxException {
+    final var lines = Files.readAllLines(Paths.get(
+        Objects.requireNonNull(getClass().getClassLoader().getResource("Day01/input.txt"))
+            .toURI()));
+    final Day01 day01 = new Day01(lines);
+    Assertions.assertThat(day01.topThreeCalories()).isEqualTo(203203);
+  }
 
 }
