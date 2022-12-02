c43ce8a1f02a10a284fe70aba5ce6a0ac9b8d43e
Greg King <greg.king@gfs.com>
2022-12-02 01:04:54 -0500

    2022 Day 02

---
+++ src/main/java/king/greg/aoc2022/Day02.java
@@ -0,0 +1,34 @@
+package king.greg.aoc2022;
+
+import java.util.List;
+
+public class Day02 {
+
+  private final List<String> rounds;
+
+  Day02(final List<String> lines) {
+    rounds = lines;
+  }
+  public long totalScore() {
+    long totalScore = 0;
+
+    for (final String round: rounds) {
+      final var them = round.charAt(0) - 'A';
+      final var me = round.charAt(2) - 'X';
+      totalScore += (((4 + me - them) % 3) * 3) + me + 1;
+    }
+    return totalScore;
+  }
+
+  public long totalDecodedScore() {
+    long totalScore = 0;
+
+    for (final String round: rounds) {
+      final var them = round.charAt(0) - 'A';
+      final var result = round.charAt(2) - 'X';
+      totalScore += (result * 3) + ((them + result + 2) % 3) + 1;
+    }
+    return totalScore;
+  }
+
+}
---
+++ src/test/java/king/greg/aoc2022/Day02Test.java
@@ -0,0 +1,49 @@
+package king.greg.aoc2022;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class Day02Test {
+
+  @Test
+  public void testSample1() throws IOException, URISyntaxException {
+    final var lines = Files.readAllLines(Paths.get(
+        Objects.requireNonNull(getClass().getClassLoader().getResource("Day02/sample.txt"))
+            .toURI()));
+    final Day02 day02 = new Day02(lines);
+    Assertions.assertThat(day02.totalScore()).isEqualTo(15);
+  }
+
+  @Test
+  public void testSolution1() throws IOException, URISyntaxException {
+    final var lines = Files.readAllLines(Paths.get(
+        Objects.requireNonNull(getClass().getClassLoader().getResource("Day02/input.txt"))
+            .toURI()));
+    final Day02 day02 = new Day02(lines);
+    Assertions.assertThat(day02.totalScore()).isEqualTo(13268);
+  }
+
+  @Test
+  public void testSample2() throws IOException, URISyntaxException {
+    final var lines = Files.readAllLines(Paths.get(
+        Objects.requireNonNull(getClass().getClassLoader().getResource("Day02/sample.txt"))
+            .toURI()));
+    final Day02 day02 = new Day02(lines);
+    Assertions.assertThat(day02.totalDecodedScore()).isEqualTo(12);
+  }
+
+  @Test
+  public void testSolution2() throws IOException, URISyntaxException {
+    final var lines = Files.readAllLines(Paths.get(
+        Objects.requireNonNull(getClass().getClassLoader().getResource("Day02/input.txt"))
+            .toURI()));
+    final Day02 day02 = new Day02(lines);
+    Assertions.assertThat(day02.totalDecodedScore()).isEqualTo(15508);
+  }
+
+}
